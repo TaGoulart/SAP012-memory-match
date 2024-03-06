2b58fa4de67ddceb6289bdc94610fa061186d6fd
TaGoulart <tamara.bgg@gmail.com>
2024-03-05 22:46:06 -0300

    fixed fisher yates

---
+++ src/components/App.js
@@ -0,0 +1,95 @@
+import SpaceData from '../data/space/space.js';
+
+const App = () => {
+  const el = document.createElement('div');
+  el.className = 'App';
+
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const randomNumber = Math.floor(Math.random() * (i + 1));
+      [array[i], array[randomNumber]] = [array[randomNumber], array[i]];
+    }
+  };
+
+  let selectedCards = [];
+  let isWaiting = false;
+
+  const createCardElements = () => {
+    const memoryGameSection = document.createElement('section');
+    memoryGameSection.className = 'memory-game';
+
+    // Filtrar o array para remover a carta 'backcard'
+    const filteredSpaceData = SpaceData.items.filter(item => item.id !== 'backcard');
+
+    // Duplicar cada carta (exceto 'backcard') para criar um par correspondente
+    const duplicatedSpaceData = [...filteredSpaceData, ...filteredSpaceData];
+
+    shuffleArray(duplicatedSpaceData); // Embaralhar o array de cartas duplicadas
+
+    duplicatedSpaceData.forEach(item => {
+      const memoryCard = createMemoryCard(item);
+      memoryGameSection.appendChild(memoryCard);
+    });
+
+    el.appendChild(memoryGameSection);
+  };
+
+  const createMemoryCard = (item) => {
+    const memoryCard = document.createElement('div');
+    memoryCard.className = 'memory-card';
+
+    const frontFace = document.createElement('img');
+    frontFace.className = 'front-face';
+    frontFace.src = item.image;
+    frontFace.alt = item.id;
+
+    const backFace = document.createElement('img');
+    backFace.className = 'back-face';
+    backFace.src = 'images/backcard.png';
+    backFace.alt = 'backcard';
+
+    memoryCard.appendChild(frontFace);
+    memoryCard.appendChild(backFace);
+
+    memoryCard.addEventListener('click', flipCard);
+
+    return memoryCard;
+  };
+
+  const flipCard = (event) => {
+    if (isWaiting) return;
+
+    const card = event.currentTarget;
+
+    if (selectedCards.includes(card)) return;
+
+    selectedCards.push(card);
+    card.classList.toggle('flip');
+
+    if (selectedCards.length === 2) {
+      isWaiting = true;
+
+      setTimeout(() => {
+        const [firstCard, secondCard] = selectedCards;
+
+        if (firstCard.children[0].alt === secondCard.children[0].alt) {
+          // Verifica se os alts das imagens são iguais
+          firstCard.classList.add('matched');
+          secondCard.classList.add('matched');
+        } else {
+          firstCard.classList.toggle('flip');
+          secondCard.classList.toggle('flip');
+        }
+
+        selectedCards = [];
+        isWaiting = false;
+      }, 1000);
+    }
+  };
+
+  createCardElements();
+
+  return el;
+};
+
+export default App;
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import App from './components/App.js';
+
+document.getElementById('root').appendChild(App());
+//document.getElementById('createCardElements').appendChild(App());
+//document.getElementById('SpaceData').appendChild(App());
+
